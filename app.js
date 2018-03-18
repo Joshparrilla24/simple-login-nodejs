b932b9b37f0808bf91b78d34c6577df356b6c7e1
joshparrilla23 <josh.parrilla24@gmail.com>
2018-03-18 23:17:04 +0800

    refactor/modularization done for repo

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ var express = require('express'),
 var app = express();
 
 // API routes
-var router = express.Router();  
+var router = express.Router(); 
+router.use(bodyParser.json()); 
+
 //api starts with /api
 app.use('/api', router);
 var connection = require('./db/db_connect');
 
 var userRepo = require('./repository/user');
+
+router.get('/user/:id', userRepo.ById);
+router.get('/users', userRepo.All);
+router.post('/user', userRepo.Add);
+router.put('/user/:id', userRepo.Update);
+router.delete('/user/:id', userRepo.Delete);
 
 var index = require('./routes/index'),
     users = require('./routes/users');
@@ -36,22 +44,25 @@ app.use(session(sessVal));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
+
+
+
--- repository/user.js
+++ repository/user.js
@@ -1,65 +1,52 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var router = express.Router();
-
-router.use(bodyParser.json());
-
 var user = require('../models/user');
-
 //API user get method by id
-router.get('/user/:id',function(req, res, next){
-  if(req.params.id){
-    user.byId(req.params.id,function(err, rows){
-      if(err){
-        res.json(err);
-      }
-      res.json(rows);
-    });
-  }
-});
-
+exports.ById = function(req, res, next){
+    if(req.params.id){
+        user.byId(req.params.id,function(err, rows){
+            if(err){
+                res.json(err);
+            }
+            res.json(rows);
+        });
+    }
+};
 // API user get all users 
-router.get('/users',function(req,res, next){
-  user.all(function(err, rows){
-    if(err){
-      res.json(err);
-    }
-    res.json(rows);
-  });
-});
-
-//API user post method
-router.post('/user', function(req, res, next){
-  user.add(req.body.user, function(err,count){
-    if(err){
-      res.json(err);
-    }
-    res.json(req.body.user);
-  });
-});
-
+exports.All = function(req,res, next){
+    user.all(function(err, rows){
+        if(err){
+            res.json(err);
+        }
+        res.json(rows);
+    });
+};
+//API add user 
+exports.Add = function(req, res, next){
+    user.add(req.body.user, function(err,count){
+        if(err){
+            res.json(err);
+        }
+        res.json(req.body.user);
+    });
+};
 //API user update
-router.put('/user/:id', function(req, res, next){
-  if(req.params.id){
-    user.update(req.params.id,req.body.user,function(err, rows){
-      if(err){
-        res.json(err);
-      }
-      res.json(rows);
-    });
-  }
-});
-
+exports.Update = function(req, res, next){
+    if(req.params.id){
+        user.update(req.params.id,req.body.user,function(err, rows){
+            if(err){
+                res.json(err);
+            }
+            res.json(rows);
+        });
+    }
+};
 //API user delete
-router.delete('/user/:id', function(req, res, next){
-  if(req.params.id){
-    user.delete(req.params.id,function(err,count){
-      if(err){
-        res.json(err);
-      }
-      res.json(count);
-    });
-  }
-});
-
-module.exports = router;
+exports.Delete = function(req, res, next){
+    if(req.params.id){
+        user.delete(req.params.id,function(err,count){
+            if(err){
+                res.json(err);
+            }
+            res.json(count);
+        });
+    }
+};
